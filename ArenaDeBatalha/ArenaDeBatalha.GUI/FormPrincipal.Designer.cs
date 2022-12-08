0a42d74b16ead669acbe27494bcc030b21c6355f
Gabriele Martins <gabriele.martinsp@gmail.com>
2022-12-08 12:34:05 -0300

    first commit

---
+++ ArenaDeBatalha/ArenaDeBatalha.GUI/FormPrincipal.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace ArenaDeBatalha.GUI
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormPrincipal
+            // 
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(830, 534);
+            this.DoubleBuffered = true;
+            this.Name = "FormPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Arena de Batalha";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FormPrincipal_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormPrincipal_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ ArenaDeBatalha/ArenaDeBatalha.GameLogic/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+namespace ArenaDeBatalha.GameLogic
+{
+    public class Enemy : GameObject
+    {
+        public Enemy(Size bounds, Graphics screenPainter, Point position) : base(bounds,screenPainter)
+        {
+            this.Left = position.X;
+            this.Top = position.Y;
+            this.Speed = 5;
+            this.Sound = Media.ExploshionShort;
+        }
+        public override Bitmap GetSprite()
+        {
+            return Media.Enemy;
+        }
+        public override void UpdadeObject()
+        {
+            this.MoveDown();
+            base.UpdadeObject();
+        }
+    }
+}
